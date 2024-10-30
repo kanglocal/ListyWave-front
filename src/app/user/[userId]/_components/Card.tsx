7cbac32d283435af111e45d310b670eeba684bf8
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-10-31 00:32:40 +0900

    Design: 마이피드 페이지 리스트 카테고리 라벨 추가 및 스타일 수정

--- src/app/user/[userId]/_components/Card.tsx
+++ src/app/user/[userId]/_components/Card.tsx
@@ -21,13 +21,17 @@ function Card({ list, isOwner, userId }: CardProps) {
   return (
     // MasonryGrid 라이브러리에서는 ul로 감싸줘야 하므로 Link태그 미사용
     <ul onClick={onClickMoveToPage(`/list/${list.id}`)} className={styles.container}>
-      {isOwner && (
-        <div className={styles.label}>
-          <div className={styles.visibilityLabel}>
+      {isOwner ? (
+        <div className={styles.labelIcon}>
+          <div className={styles.label}>
             {!list.isPublic && <LockIcon fill={vars.color.blue} />}
             <span>{list.isPublic ? '공개' : '비공개'}</span>
           </div>
           <OptionToggleButton listId={String(list.id)} userId={userId} isPublicCurrent={list.isPublic} />
+        </div>
+      ) : (
+        <div className={styles.label}>
+          <span>{list.category}</span>
         </div>
       )}
       <h2 className={styles.title}>{list.title}</h2>
--- src/app/user/[userId]/_components/Content.css.ts
+++ src/app/user/[userId]/_components/Content.css.ts
@@ -12,7 +12,8 @@ export const container = style({
 });
 
 export const contentInfo = style({
-  padding: '1.65rem 1.6rem 0.65rem 1.9rem',
+  height: 32,
+  margin: '1.65rem 1.6rem 0.65rem 1.9rem',
 
   display: 'flex',
   justifyContent: 'space-between',
