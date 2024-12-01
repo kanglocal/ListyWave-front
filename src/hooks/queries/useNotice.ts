dd44db80eb600695c615e220a2cb6e10a2b14b72
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-12-01 20:47:55 +0900

    Style: useNotice 커스텀 훅 함수 오타 수정

--- src/hooks/queries/useNotice.ts
+++ src/hooks/queries/useNotice.ts
@@ -26,19 +26,19 @@ const useNoticeMutation = <TData = unknown, TError = unknown, TVariables = unkno
 // useNoticeMutation이 반환하는 타입은 UseMutationResult<TData, TError, TVariables, unknown> 타입으로,
 // useNotice 훅이 반환하는 각 함수의 반환값(void), 에러, 변수 타입(number), context 타입을 정의해주었다.
 type UseNoticeReturnType = {
-  deletNoticeMutation: UseMutationResult<void, unknown, number, unknown>;
+  deleteNoticeMutation: UseMutationResult<void, unknown, number, unknown>;
   sendNoticeAlarmMutation: UseMutationResult<void, unknown, number, unknown>;
   updateNoticePublicMutation: UseMutationResult<void, unknown, number, unknown>;
 };
 
 // React-query의 useMutation을 wrapping해주는 Notice 관련 custom hook
 const useNotice = (): UseNoticeReturnType => {
-  const deletNoticeMutation = useNoticeMutation(deleteNotice);
+  const deleteNoticeMutation = useNoticeMutation(deleteNotice);
   const sendNoticeAlarmMutation = useNoticeMutation(sendNoticeAlarm);
   const updateNoticePublicMutation = useNoticeMutation(updateNoticePublic);
 
   return {
-    deletNoticeMutation,
+    deleteNoticeMutation,
     sendNoticeAlarmMutation,
     updateNoticePublicMutation,
   };
